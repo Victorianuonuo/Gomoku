9688aca015b6ecb5e4aa16f945042b01d68be089
Victoria <oysq@ruc.edu.cn>
2016-06-11 16:20:21 +0800

    delete unrelevent code in Rivals

--- src/Rivals.java
+++ src/Rivals.java
@@ -28,23 +28,6 @@ public class Rivals {
         return name + " at " + address ;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Rivals rival = (Rivals) o;
-
-        if (address != null ? !address.equals(rival.address) : rival.address != null) return false;
-        return !(name != null ? !name.equals(rival.name) : rival.name != null);
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = address != null ? address.hashCode() : 0;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        return result;
-    }
+ 
 	
 }
